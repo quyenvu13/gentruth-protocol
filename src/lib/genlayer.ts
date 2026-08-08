49918b75bff831237c7699dc31655235c2e3bd13
quyenvu13 <195855187+quyenvu13@users.noreply.github.com>
2026-08-08 23:40:52 +0700

    Delete src directory

--- src/lib/genlayer.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "genlayer-js";
-
-// The deployed GenTruth Oracle Smart Contract Address
-export const CONTRACT_ADDRESS = "0x84ec259564713b766F1B093905Cded802Abfb8F3";
-
-// Configure GenLayer SDK Client (Studionet environment)
-export const client = createClient({
-    endpoint: "https://studio.genlayer.com:7182"
-});
